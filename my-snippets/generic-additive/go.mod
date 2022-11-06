5a9d0fcde5c13df6f4c47faeea6e2341e2f1a6a3
valteem <val.teem.scn@gmail.com>
2022-11-06 17:55:55 +0300

    added example of additive generics

---
+++ my-snippets/generic-additive/go.mod
@@ -0,0 +1,5 @@
+module my.golang.play/generic-additive
+
+go 1.19
+
+require golang.org/x/exp v0.0.0-20221106115401-f9659909a136
---
+++ my-snippets/generic-additive/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
+
+/* func Scale[S []~E, E int](s S, sc E) S{
+    r := make(S, len(s))
+    for i, v := range s {
+        r[i] = v * c
+    }
+
+    return r
+} */
+
+type additive interface {
+	int|uint32|uint64|float32|float64
+}
+
+func add[T additive](a T, b T) T {
+	return a + b
+}
+
+func addConstraints[T constraints.Ordered](a T, b T) T {
+	return a + b
+}
+
+func main() {
+
+	a_int, b_int := 1, 2
+	fmt.Println(add(a_int, b_int), addConstraints(a_int, b_int))
+	a_uint32, b_uint32 := 1, 2
+	fmt.Println(add(a_uint32, b_uint32), addConstraints(a_uint32, b_uint32))
+	a_uint64, b_uint64 := 1, 2
+	fmt.Println(add(a_uint64, b_uint64), addConstraints(a_uint64, b_uint64))
+	a_float32, b_float32 := 1.0, 2.0
+	fmt.Println(add(a_float32, b_float32), addConstraints(a_float32, b_float32))
+	a_float64, b_float64 := 1.0, 2.0
+	fmt.Println(add(a_float64, b_float64), addConstraints(a_float64, b_float64))
+
+}
