0c3341bb0d15805f15fe0978a58c13e7c851e34b
valteem <val.teem.scn@gmail.com>
2023-07-09 19:11:50 +0300

    added random slice generatiobn for smart/brute insertion sort example

--- clrs-algo/insertion-sort-generic/main.go
+++ clrs-algo/insertion-sort-generic/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	nmax = 100
+	num = 15
 )
 
 func main() {
 
-	s:= []int{17, 4, 11, 7, 19, 3, 14, 8, 15, 44, 33, 27, 51, 8}
+	s1 := make([]int, num)
+	s2 := make([]int, num)
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < num; i++ {
+		s1[i] = rand.Intn(nmax)
+		s2[i] = s1[i]
+	}
 	
-	smartSort(s)
-	fmt.Println(s)
+	fmt.Println(s1)
+	smartSort(s1)
+	fmt.Println(s1)
+	bruteSort(s2)
+	fmt.Println(s2)
 	
 }
 
