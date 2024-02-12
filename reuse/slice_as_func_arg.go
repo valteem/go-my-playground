ce32018af89e57175f13eecd6c2d09ea7654b303
valteem <val.teem.scn@gmail.com>
2024-02-12 15:47:16 +0300

    convert slice-as-function-argument example to real test
    add passing pointer to a slice as argument

--- reuse/slice_as_func_arg.go
+++ reuse/slice_as_func_arg.go
@@ -1,7 +1,11 @@
 package reuse
 
-func AppendToSlice[T any](s []T, e T) {
-	s = append(s, e)
+func AppendToSlice[T any](s []T, a T) {
+	s = append(s, a)
+}
+
+func AppendToSliceP[T any](s *[]T, a T) {
+	*s = append(*s, a) // (*s) is a slice, since s is a pointer (?)
 }
 
 type SliceContainer [T any] struct {
@@ -16,10 +20,21 @@ func (sc SliceContainer[T]) Slice() []T {
 	return sc.s
 }
 
-func (sc SliceContainer[T])AppendToSlice(e T) {
-	sc.s = append(sc.s, e)
+func (sc SliceContainer[T])AppendToSlice(a T) {
+	sc.s = append(sc.s, a)
 }
 
-func (sc *SliceContainer[T])AppendToSliceP(e T) {
-	sc.s = append(sc.s, e)
+func (sc *SliceContainer[T])AppendToSliceP(a T) {
+	sc.s = append(sc.s, a)
+}
+
+// utility func for testing
+func SlicesEqual[T comparable](s1 []T, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := 0; i <  len(s1); i++ {
+		if s1[i] != s2[i] {return false}
+	}
+	return true
 }
--- reuse/slice_as_func_arg_test.go
+++ reuse/slice_as_func_arg_test.go
@@ -1,7 +1,6 @@
 package reuse_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/valteem/reuse"
@@ -11,14 +10,30 @@ func TestAppendToSlice(t *testing.T) {
 	
 	s := []int{1, 2, 3, 4}
 	reuse.AppendToSlice[int](s, 5)
-	fmt.Println(s) // [1 2 3 4]
+	e := []int{1, 2, 3, 4} // nothind appended, slice is passed by value
+	if !reuse.SliceEqual(s, e) {
+		t.Errorf("Expected %+v, returned %+v", e, s)
+	}
+
+	s0:= []int{1, 2, 3, 4}
+	reuse.AppendToSliceP[int](&s0, 5)
+	e0 := []int{1, 2, 3, 4, 5}
+	if !reuse.SliceEqual(s0, e0) {
+		t.Errorf("Expected %+v, returned %+v", e0, s0)
+	}
 
 	c1 := reuse.NewSliceContainer[int]([]int{1, 2, 3, 4})
 	c1.AppendToSlice(5)
-	fmt.Println(c1.Slice()) // [1 2 3 4]
+	e1 := []int{1, 2, 3, 4} // nothind appended, function defined on SliceContainer struct, not pointer
+	if !reuse.SliceEqual(c1.Slice(), e1) {
+		t.Errorf("Expected %+v, returned %+v", e1, c1.Slice())
+	}
 
 	c2 := reuse.NewSliceContainer[int]([]int{1, 2, 3, 4})
 	c2.AppendToSliceP(5)
-	fmt.Println(c2.Slice()) // [1 2 3 4 5]
+	e2 := []int{1, 2, 3, 4, 5}
+	if !reuse.SliceEqual(c2.Slice(), e2) {
+		t.Errorf("Expected %+v, returned %+v", e2, c2.Slice())
+	}
 
 }
