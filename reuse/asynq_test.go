672df12eac6bdf00858cc5ca3b102280b6eed736
valteem <val.teem.scn@gmail.com>
2024-02-18 07:24:04 +0300

    simple test for 'hibiken-asynq' task

---
+++ reuse/asynq_test.go
@@ -0,0 +1,18 @@
+package reuse
+
+import (
+
+	"testing"
+
+	"github.com/hibiken/asynq"
+)
+
+func TestAsynq(t *testing.T) {
+
+	ttype := "task"
+	task := asynq.NewTask(ttype, []byte("payload"))
+
+	if task.Type() != "task" {
+		t.Errorf("task type shoud be '%+v', is '%v'", ttype, task.Type())
+	}
+}
