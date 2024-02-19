42da515d5378965739b53806aef7ac82b57050e1
valteem <val.teem.scn@gmail.com>
2024-02-19 08:19:13 +0300

    modify immutable-radix-tree example

--- reuse/iradix_test.go
+++ reuse/iradix_test.go
@@ -8,13 +8,16 @@ import (
 
 func TestBasicInsert(t *testing.T) {
 	r := iradix.New[int]()
-	r, _, _ = r.Insert([]byte("abc"), 11)
-	r, oldValue, isSet := r.Insert([]byte("abc"), 12)
-	if oldValue != 11 {
+	r, _, _ = r.Insert([]byte("a"), 1)
+	r, _, _ = r.Insert([]byte("ab"), 2)
+	r, _, _ = r.Insert([]byte("ax"), 102)
+	r, _, _ = r.Insert([]byte("abc"), 3)
+	r, oldValue, isSet := r.Insert([]byte("abc"), 33)
+	if oldValue != 3 {
 		t.Errorf("Old value should be 11, is %+v", oldValue)
 	}
-	if r.Len() != 1{
-		t.Errorf("Tree length should be 1, is %+v", r.Len())
+	if r.Len() != 4{
+		t.Errorf("Tree length should be 4, is %+v", r.Len())
 	}
 	if isSet != true {
 		t.Errorf("Is Set flag should be true, is %+v", isSet)
