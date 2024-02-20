31b5f0b6c88a26dc7f77f77451d44b6ccfe99dd6
valteem <val.teem.scn@gmail.com>
2024-02-20 15:03:53 +0300

    ATQ: Redis client options (regular, failover, cluster)

--- atq/atq.go
+++ atq/atq.go
@@ -5,15 +5,15 @@ import (
 	"crypto/tls"
 	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/valteem/atq/internal/base"
 )
 
 type ResultWriter struct {
-	id     string // task ID
-	qname  string // qeueu name this task belongs to
-	broker any // base.Broker /internal/base
-	// TODO: find SO thread about packing context inside struct considered a bad idea
-	ctx    context.Context
+	id     string          // task ID
+	qname  string          // qeueu name this task belongs to
+	broker any             // base.Broker /internal/base
+	ctx    context.Context // TODO: find SO thread about packing context inside struct considered a bad idea
 }
 
 type Task struct {
@@ -24,11 +24,11 @@ type Task struct {
 }
 
 func (t *Task) Type() string                { return t.typename }
-func (t *Task) Payload() []byte             { return t.payload  }
-func (t *Task) ResultWriter() *ResultWriter {return t.w} // nil ptr on newly created task
+func (t *Task) Payload() []byte             { return t.payload }
+func (t *Task) ResultWriter() *ResultWriter { return t.w } // nil ptr on newly created task
 
 // Public constructor: accepts task type name, payload, options
-func NewTask(typename string, payload[]byte, opts ...Option) *Task {
+func NewTask(typename string, payload []byte, opts ...Option) *Task {
 	return &Task{
 		typename: typename,
 		payload:  payload,
@@ -54,16 +54,16 @@ const (
 	TaskStateRetry
 	TaskStateArchived
 	TaskStateCompleted
-	TaskStateAggregating // waiting in a group to be aggregated into single task 
+	TaskStateAggregating // waiting in a group to be aggregated into single task
 )
 
 var taskStateString = map[TaskState]string{
-	TaskStateActive: "active",
-	TaskStatePending: "pending",
-	TaskStateScheduled: "scheduled",
-	TaskStateRetry: "retry",
-	TaskStateArchived: "archived",
-	TaskStateCompleted: "completed",
+	TaskStateActive:      "active",
+	TaskStatePending:     "pending",
+	TaskStateScheduled:   "scheduled",
+	TaskStateRetry:       "retry",
+	TaskStateArchived:    "archived",
+	TaskStateCompleted:   "completed",
 	TaskStateAggregating: "aggregating",
 }
 
@@ -77,30 +77,30 @@ func (s TaskState) String() string {
 }
 
 type TaskInfo struct {
-	ID string // task identifier (missing in Task definition?!)
-	Queue string
-	Type string // typename?
-	Payload []byte
-	State TaskState
-	MaxRetry int
-	Retried int
-	LastErr string // error message from last failure
-	LastFailedAt time.Time
-	Timeout time.Duration // time before being retried
-	Deadline time.Time // zero if not set
-	Group string
-	NextProcessAt time.Time // scheduled processing time, zero if not applicable
-	IsOrphaned bool // active task with no worker processing it (worker crash, network failure, etc)
-	Retention time.Duration // time after task is successfully processed
-	CompletedAt time.Time
-	Result []byte
+	ID            string // task identifier (missing in Task definition?!)
+	Queue         string
+	Type          string // typename?
+	Payload       []byte
+	State         TaskState
+	MaxRetry      int
+	Retried       int
+	LastErr       string // error message from last failure
+	LastFailedAt  time.Time
+	Timeout       time.Duration // time before being retried
+	Deadline      time.Time     // zero if not set
+	Group         string
+	NextProcessAt time.Time     // scheduled processing time, zero if not applicable
+	IsOrphaned    bool          // active task with no worker processing it (worker crash, network failure, etc)
+	Retention     time.Duration // time after task is successfully processed
+	CompletedAt   time.Time
+	Result        []byte
 }
 
 // Convert Unix time (int64 in seconds since 1970-1-1 00:00:00) to time.Time
 func fromUnixTimeOrZero(t int64) time.Time {
 	if t == 0 {
 		return time.Time{} // 'real' zero time (time.Time(0001-01-01T00:00:00Z))
-		                   // instead of Unix zero time (time.Time(1970-01-01T00:00:00Z))
+		// instead of Unix zero time (time.Time(1970-01-01T00:00:00Z))
 	}
 	return time.Unix(t, 0)
 }
@@ -108,23 +108,22 @@ func fromUnixTimeOrZero(t int64) time.Time {
 // TODO: replace (if really needed) TaskSate with base.TaskState, both look the same
 func newTaskInfo(msg base.TaskMessage, state TaskState, nextProcessAt time.Time, result []byte) *TaskInfo {
 	info := TaskInfo{
-		ID: msg.ID,
-		Queue: msg.Queue,
-		Type: msg.Type,
-		Payload: msg.Payload,
-		MaxRetry: msg.Retry,
-		Retried: msg.Retried,
-		LastErr: msg.ErrorMsg,
-		Group: msg.GroupKey,
-		Timeout: time.Duration(msg.Timeout) * time.Second,
-		Deadline: fromUnixTimeOrZero(msg.Deadline),
-		Retention: time.Duration(msg.Retention) * time.Second,
+		ID:            msg.ID,
+		Queue:         msg.Queue,
+		Type:          msg.Type,
+		Payload:       msg.Payload,
+		MaxRetry:      msg.Retry,
+		Retried:       msg.Retried,
+		LastErr:       msg.ErrorMsg,
+		Group:         msg.GroupKey,
+		Timeout:       time.Duration(msg.Timeout) * time.Second,
+		Deadline:      fromUnixTimeOrZero(msg.Deadline),
+		Retention:     time.Duration(msg.Retention) * time.Second,
 		NextProcessAt: nextProcessAt,
-		LastFailedAt: fromUnixTimeOrZero(msg.LastFailedAt),
-		CompletedAt: fromUnixTimeOrZero(msg.CompletedAt),
-		Result: result,
-		// TODO: replace with switch-case if base.TaskState is used instead
-		State: state,
+		LastFailedAt:  fromUnixTimeOrZero(msg.LastFailedAt),
+		CompletedAt:   fromUnixTimeOrZero(msg.CompletedAt),
+		Result:        result,
+		State:         state, // TODO: replace with switch-case if base.TaskState is used instead
 	}
 	return &info
 }
@@ -138,14 +137,83 @@ type RedisConnOpt interface {
 }
 
 type RedisClientOpt struct {
-	Network string // default TCP
-	Addr string // host:port
-	Username string
-	Password string
-	DB int // Redis DB to select after connecting to a server
-	DialTimeout time.Duration // establish new connection, default 5 sec
-	ReadTimeout time.Duration // socket reads, default 3 sec
+	Network      string // default TCP
+	Addr         string // host:port
+	Username     string
+	Password     string
+	DB           int           // Redis DB to select after connecting to a server
+	DialTimeout  time.Duration // establish new connection, default 5 sec
+	ReadTimeout  time.Duration // socket reads, default 3 sec
 	WriteTimeout time.Duration //socket writes, default 3 sec
-	PoolSize int // max number of socket connections, default 10 per CPU
-	TLSConfig *tls.Config // TLS negotiated only if this field is set
+	PoolSize     int           // max number of socket connections, default 10 per CPU
+	TLSConfig    *tls.Config   // TLS negotiated only if this field is set
+}
+
+func (opt RedisClientOpt) MakeRedisClient() any {
+	return redis.NewClient(&redis.Options{
+		Network:      opt.Network,
+		Addr:         opt.Addr,
+		Username:     opt.Username,
+		Password:     opt.Password,
+		DB:           opt.DB,
+		DialTimeout:  opt.DialTimeout,
+		ReadTimeout:  opt.ReadTimeout,
+		WriteTimeout: opt.WriteTimeout,
+		PoolSize:     opt.PoolSize,
+		TLSConfig:    opt.TLSConfig,
+	})
+}
+
+type RedisFailoverClientOpt struct {
+	MasterName       string   // 'master' monitored by 'sentinels'
+	SentinelAddrs    []string // host:port
+	SentinelPassword string
+	Username         string
+	Password         string
+	DB               int
+	DialTimeout      time.Duration
+	ReadTimeout      time.Duration
+	WriteTimeout     time.Duration
+	PoolSize         int
+	TLSConfig        *tls.Config
+}
+
+func (opt RedisFailoverClientOpt) MakeRedisClient() any {
+	return redis.NewFailoverClient(&redis.FailoverOptions{
+		MasterName:       opt.MasterName,
+		SentinelAddrs:    opt.SentinelAddrs,
+		SentinelPassword: opt.SentinelPassword,
+		Username:         opt.Username,
+		Password:         opt.Password,
+		DB:               opt.DB,
+		DialTimeout:      opt.DialTimeout,
+		ReadTimeout:      opt.ReadTimeout,
+		WriteTimeout:     opt.WriteTimeout,
+		PoolSize:         opt.PoolSize,
+		TLSConfig:        opt.TLSConfig,
+	})
+}
+
+type RedisClusterClientOpt struct {
+	Addrs        []string // host:port addresses of cluster node
+	MaxRedirects int
+	Username     string
+	Password     string
+	DialTimeout  time.Duration
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	TLSConfig    *tls.Config
+}
+
+func (opt RedisClusterClientOpt) MakeRedisClient() any {
+	return redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:        opt.Addrs,
+		MaxRedirects: opt.MaxRedirects,
+		Username:     opt.Username,
+		Password:     opt.Password,
+		DialTimeout:  opt.DialTimeout,
+		ReadTimeout:  opt.ReadTimeout,
+		WriteTimeout: opt.WriteTimeout,
+		TLSConfig:    opt.TLSConfig,
+	})
 }
