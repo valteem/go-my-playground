dd51b0ef50292170b5a5b4a0427a259116314a28
valteem <val.teem.scn@gmail.com>
2024-02-21 09:02:50 +0300

    parse URL path - remove leading and trailing slashes, split to segments

--- reuse/parse_url_test.go
+++ reuse/parse_url_test.go
@@ -2,6 +2,7 @@ package reuse
 
 import (
 	"net/url"
+	"strings"
 
 	"testing"
 )
@@ -16,5 +17,11 @@ func TestToURL(t *testing.T) {
 	u, _ := url.Parse(addr) // retuns pointer
 	if *u != parsedURL {
 		t.Errorf("URL representation should be %+v, is %+v", parsedURL, u)
+	}
+
+	s := strings.Trim(u.Path, "/") // remove leading and trailing slashes
+	p := strings.Split(s, "/")
+	if p[0] != "questions" {
+		t.Errorf("wrong split result")
 	}
 }
