f747faa2d2c73bcbca2cde5bf80fe74912311a95
valteem <val.teem.scn@gmail.com>
2024-03-09 13:24:51 +0300

    add implementation test for value receiver

--- reuse/implement_test.go
+++ reuse/implement_test.go
@@ -29,9 +29,22 @@ func (b *AnotherBroker) Close() string {
 	return "closed"
 }
 
+type YetAnotherBrocker struct{}
+
+func (b YetAnotherBrocker) Ping() error {
+	return nil
+}
+
+func (b YetAnotherBrocker) Close() error {
+	return nil
+}
+
 func TestBrokerImplementation(t *testing.T) {
 
 	var _ Broker = (*TestBroker)(nil) // converting 'nil' to type '*TestBroker' (https://stackoverflow.com/a/60440959)
+
+	var s struct{}
+	var _ Broker = (YetAnotherBrocker)(s) // test implementation with 'value' receiver
 
 	// Does not compile - wrong implementation
 	// var _ Broker = (*AnotherBroker)(nil)
