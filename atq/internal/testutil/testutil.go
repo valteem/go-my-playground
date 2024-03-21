23282273f2048fc65b6c8d0a3cf81a183ebdddaf
valteem <val.teem.scn@gmail.com>
2024-03-21 13:06:29 +0300

    ATQ: internal/testutil SortMsgOpt

--- atq/internal/testutil/testutil.go
+++ atq/internal/testutil/testutil.go
@@ -2,8 +2,10 @@ package testutil
 
 import (
 	"math"
+	"sort"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/valteem/atq/internal/base"
 )
 
 func EquateInt64Approx(margin int64) cmp.Option {
@@ -11,3 +13,12 @@ func EquateInt64Approx(margin int64) cmp.Option {
 		return math.Abs(float64(i-j)) <= float64(margin)
 	})
 }
+
+// cmp.Option to compare two slices of task messages
+var SortMsgOpt = cmp.Transformer("SortTaskMessages", func(in []*base.TaskMessage) []*base.TaskMessage {
+	out := append([]*base.TaskMessage(nil), in...) //avoid mutating input
+	sort.Slice(out, func(i, j int) bool {
+		return out[i].ID < out[j].ID
+	})
+	return out
+})
--- atq/internal/testutil/testutil_test.go
+++ atq/internal/testutil/testutil_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/valteem/atq/internal/base"
 )
 
 func TestEquateInt64ApproxEqual(t *testing.T) {
@@ -73,5 +74,90 @@ func TestEquateInt64ApproxNotEqual(t *testing.T) {
 			t.Errorf("%s: %v cannot be equal %v within margin %v", tst.description, tst.v1, tst.v2, tst.margin)
 		}
 
+	}
+}
+
+func TestSortMsgOptEqual(t *testing.T) {
+	tests := []struct {
+		description   string
+		taskMessagesA []*base.TaskMessage
+		taskMessagesB []*base.TaskMessage
+	}{
+		{
+			description: "sort order ascending vs descending",
+			taskMessagesA: []*base.TaskMessage{
+				{ID: "11"}, // https://pkg.go.dev/cmd/gofmt#hdr-The_simplify_command
+				{ID: "21"},
+				{ID: "31"},
+			},
+			taskMessagesB: []*base.TaskMessage{
+				{ID: "31"},
+				{ID: "21"},
+				{ID: "11"},
+			},
+		},
+		{
+			description: "shuffled order",
+			taskMessagesA: []*base.TaskMessage{
+				{ID: "31"},
+				{ID: "21"},
+				{ID: "41"},
+				{ID: "11"},
+			},
+			taskMessagesB: []*base.TaskMessage{
+				{ID: "21"},
+				{ID: "11"},
+				{ID: "41"},
+				{ID: "31"},
+			},
+		},
+	}
+	for _, tst := range tests {
+		if !cmp.Equal(tst.taskMessagesA, tst.taskMessagesB, SortMsgOpt) {
+			t.Errorf("%s:\n%v\n%v\nshould be equal", tst.description, tst.taskMessagesA, tst.taskMessagesB)
+		}
+	}
+}
+
+func TestSortMsgOptNotEqual(t *testing.T) {
+	tests := []struct {
+		description   string
+		taskMessagesA []*base.TaskMessage
+		taskMessagesB []*base.TaskMessage
+	}{
+		{
+			description: "same length, different values",
+			taskMessagesA: []*base.TaskMessage{
+				{ID: "11"},
+				{ID: "21"},
+				{ID: "31"},
+			},
+			taskMessagesB: []*base.TaskMessage{
+				{ID: "12"},
+				{ID: "22"},
+				{ID: "32"},
+			},
+		},
+		{
+			description: "different length",
+			taskMessagesA: []*base.TaskMessage{
+				{ID: "31"},
+				{ID: "21"},
+				{ID: "41"},
+				{ID: "11"},
+			},
+			taskMessagesB: []*base.TaskMessage{
+				{ID: "21"},
+				{ID: "11"},
+				{ID: "41"},
+				{ID: "31"},
+				{ID: "51"},
+			},
+		},
+	}
+	for _, tst := range tests {
+		if cmp.Equal(tst.taskMessagesA, tst.taskMessagesB, SortMsgOpt) {
+			t.Errorf("%s:\n%v\n%v\nshould be equal", tst.description, tst.taskMessagesA, tst.taskMessagesB)
+		}
 	}
 }
