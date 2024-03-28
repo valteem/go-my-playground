f3d462ab0f5b0f4473cc33e0276f05c7f00905e0
valteem <val.teem.scn@gmail.com>
2024-03-28 14:23:19 +0300

    elaborate on converting uint to int

--- reuse/int_conversion_test.go
+++ reuse/int_conversion_test.go
@@ -39,10 +39,38 @@ func TestIntConversion(t *testing.T) {
 			},
 			formattedValue: "10",
 		},
+		{
+			description: "uint8, decimal",
+			value:       int8(-16),
+			formatFunc: func(value any) string {
+				v := value.(int8)
+				return strconv.FormatUint(uint64(v), 10)
+			},
+			formattedValue: strconv.FormatUint(^uint64(0)-16+1, 10),
+		},
 	}
 	for _, tst := range tests {
 		if v := tst.formatFunc(tst.value); v != tst.formattedValue {
 			t.Errorf("%s: get %v, expect %v", tst.description, v, tst.formattedValue)
+		}
+	}
+}
+
+func TestIntToUint(t *testing.T) {
+	tests := []struct {
+		description string
+		input       uint8
+		//		convInput   func(uint8) int8
+		output int8
+	}{
+		{
+			input:  255,
+			output: -1, // 255 - 128 = 127, -128 + 127 = -1
+		},
+	}
+	for _, tst := range tests {
+		if v := int8(tst.input); v != tst.output {
+			t.Errorf("%s, get %d, expect %d", tst.description, v, tst.output)
 		}
 	}
 }
