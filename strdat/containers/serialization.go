824ef2d6b4c80aa3d722ccf82dfd0689b74022fe
valteem <val.teem.scn@gmail.com>
2024-04-03 05:53:07 +0300

    strdat: container/serialization interfaces

---
+++ strdat/containers/serialization.go
@@ -0,0 +1,11 @@
+package containers
+
+type JSONSerializer interface {
+	ToJSON() ([]byte, error)      // JSON representation of the content of the container
+	MarshalJSON() ([]byte, error) // implements json.Marshaler
+}
+
+type JSONDeserialization interface {
+	FromJSON([]byte) error      // populates container from input JSON
+	UnmarshalJSON([]byte) error // implements json.Unmarshaler
+}
