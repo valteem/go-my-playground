8573e6c8d7b7e42382306ae0e034f0dd34a48ba3
valteem <val.teem.scn@gmail.com>
2024-05-20 16:41:08 +0300

    strdat/stacks: fix Stack{} signature
    Pop(), Peek()

--- strdat/stacks/stacks.go
+++ strdat/stacks/stacks.go
@@ -4,8 +4,8 @@ import "github.com/valteem/strdat/containers"
 
 type Stack[T any] interface {
 	Push(value T)
-	Pop(value T, ok bool)
-	Peek(value T, ok bool)
+	Pop() (value T, ok bool)
+	Peek() (value T, ok bool)
 
 	containers.Container[T]
 }
