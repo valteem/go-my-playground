f652cbcc458f5376814427e1b640dc6c8675d2c7
valteem <val.teem.scn@gmail.com>
2024-06-06 17:18:45 +0300

    timeseries/common/metric: add descriptions to validation and escaping schemes

--- timeseries/common/model/metric.go
+++ timeseries/common/model/metric.go
@@ -4,10 +4,13 @@ var (
 	NameValidationScheme = LegacyValidation
 )
 
+// Defines how metric and label names are validated
 type ValidationScheme int
 
 const (
+	// Requires that metric and label names conform to MetricNameRE
 	LegacyValidation ValidationScheme = iota
+	// Requires that metric and label names are valid UTF-8 strings
 	UTF8Validation
 )
 
@@ -15,7 +18,11 @@ type EscapingScheme int
 
 const (
 	NoEscaping EscapingScheme = iota
+	// Replaces all legacy-invalid (?) characters with underscores
 	UnderscoreEscaping
+	// Same as UnderscoreEscaping, except that dots are replace with `_dot_`, and single underscores converted to double underscores
 	DotsEscaping
+	// Prepends the name with `U_`, replaces all invalid (?) characters with unicode value, surrounded by underscores
+	// Replaces single underscores with double underscores
 	ValueEncodingEscaping
 )
