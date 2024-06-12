08d7f53c5c6154c9d26d7c9a84513e994d2b11bd
valteem <val.teem.scn@gmail.com>
2024-06-12 15:31:18 +0300

    reuse: benchmark regular and fast labels-to-signature options
    single make() allocation leads to 2x performance loss

--- reuse/fnv_test.go
+++ reuse/fnv_test.go
@@ -52,3 +52,97 @@ func TestMapToFNV(t *testing.T) {
 		}
 	}
 }
+
+// The following is set to benchmark two FNV hashing algorithms, regular and `fast`
+
+const (
+	offset64 = 14695981039346656037
+	prime64  = 1099511628211
+)
+
+var (
+	emptyLabelSignature = hashNew()
+)
+
+// hashNew initializes a new fnv64a hash value.
+func hashNew() uint64 {
+	return offset64
+}
+
+// hashAdd adds a string to a fnv64a hash value, returning the updated hash.
+func hashAdd(h uint64, s string) uint64 {
+	for i := 0; i < len(s); i++ {
+		h ^= uint64(s[i])
+		h *= prime64
+	}
+	return h
+}
+
+// hashAddByte adds a byte to a fnv64a hash value, returning the updated hash.
+func hashAddByte(h uint64, b byte) uint64 {
+	h ^= uint64(b)
+	h *= prime64
+	return h
+}
+
+func labelsToSignature(labels map[string]string) uint64 {
+	if len(labels) == 0 {
+		return emptyLabelSignature
+	}
+
+	labelNames := make([]string, 0, len(labels)) // allocation
+	for labelName := range labels {
+		labelNames = append(labelNames, labelName)
+	}
+	sort.Strings(labelNames)
+
+	sum := hashNew()
+	for _, labelName := range labelNames {
+		sum = hashAdd(sum, labelName)
+		sum = hashAddByte(sum, separatorByte)
+		sum = hashAdd(sum, labels[labelName])
+		sum = hashAddByte(sum, separatorByte)
+	}
+	return sum
+}
+
+func labelsToSignatureFast(m map[string]string) uint64 {
+	if len(m) == 0 {
+		return emptyLabelSignature
+	}
+
+	var result uint64
+	for labelName, labelValue := range m {
+		sum := hashNew()
+		sum = hashAdd(sum, labelName)
+		sum = hashAddByte(sum, separatorByte)
+		sum = hashAdd(sum, labelValue)
+		result ^= sum
+	}
+	return result
+}
+
+// go test fnv_test.go -gcflags -'-m=3 -l'
+func BenchmarkLabelsToSignature(b *testing.B) {
+	m := map[string]string{
+		"label1":               "value1",
+		"label2":               "value2",
+		"label3":               "value3",
+		"some random label":    "some random value",
+		"another random label": "another random vaule",
+		"other label stuff":    "other value stuff",
+		"more somple labels":   "more simple values",
+	}
+
+	b.Run("Regular labels-to-signature", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = labelsToSignature(m)
+		}
+	})
+
+	b.Run("Fast labels-to-signature", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = labelsToSignatureFast(m)
+		}
+	})
+}
