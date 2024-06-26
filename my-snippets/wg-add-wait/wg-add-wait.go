f786b28df196e3da3e6e14415ba95bcdf325b2ff
valteem <val.teem.scn@gmail.com>
2024-06-26 15:51:03 +0300

    fix WaitGroup example

--- my-snippets/wg-add-wait/wg-add-wait.go
+++ my-snippets/wg-add-wait/wg-add-wait.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"sync"
-	"unsafe"
 )
 
 func worker(nw int, nj int, wg *sync.WaitGroup) {
 
-	for count := 1; count <= nj; count ++ {
-        fmt.Printf("Worker #%d performing job #%d\n", nw, count)
+	for count := 1; count <= nj; count++ {
+		fmt.Printf("Worker #%d performing job #%d\n", nw, count)
 	}
 	wg.Done()
 }
 
 func main() {
-	
+
 	numWorkers := 15
 	numJobs := 2
 
@@ -27,5 +26,6 @@ func main() {
 	}
 
 	wg.Wait()
+	fmt.Print("Completed")
 
 }
