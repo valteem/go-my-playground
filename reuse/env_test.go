6b39b34086b63e8bd8fe036165c8c34de323122d
valteem <val.teem.scn@gmail.com>
2024-06-30 19:31:28 +0300

    reuse: set/get environment variable

---
+++ reuse/env_test.go
@@ -0,0 +1,19 @@
+package reuse_test
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSetEnv(t *testing.T) {
+	envVarName := "SOME_ENV_VAR"
+	envVarValue := "TRUE"
+	err := os.Setenv(envVarName, envVarValue)
+	if err != nil {
+		t.Errorf("Failes to set env variable: %q", err)
+	}
+	actualEnvVarValue := os.Getenv(envVarName)
+	if actualEnvVarValue != envVarValue {
+		t.Errorf("os.getenv(%s): get %s, expect %s", envVarName, actualEnvVarValue, envVarValue)
+	}
+}
