db96735ee639921aa5d17da9b9f3cb577adc7ba4
valteem <val.teem.scn@gmail.com>
2024-08-03 11:33:15 +0300

    reuse: elaborate on return-argument example, test slice return, compare to std append()

--- reuse/return_arg_test.go
+++ reuse/return_arg_test.go
@@ -1,6 +1,8 @@
 package reuse_test
 
 import (
+	"fmt"
+	"slices"
 	"testing"
 )
 
@@ -12,6 +14,11 @@ func changeNoEffect(value int) {
 func changeEffect(value int) int {
 	value += 1
 	return value
+}
+
+func changeSliceAndReturn(s []int) []int {
+	s = append(s, 1)
+	return s
 }
 
 func TestReturnArg(t *testing.T) {
@@ -26,6 +33,30 @@ func TestReturnArg(t *testing.T) {
 	v2 = changeEffect(v2)
 	if v2 != 2 {
 		t.Errorf("changeEffect(): get %d, expect 2", v2)
+	}
+
+}
+
+func TestReturnSlice(t *testing.T) {
+
+	s := []int{0}
+	pAddrBefore := fmt.Sprintf("%p", &s[0])
+	s = changeSliceAndReturn(s) // returns new slice
+	pAddrAfter := fmt.Sprintf("%p", &s[0])
+	if !slices.Equal(s, []int{0, 1}) {
+		t.Errorf("changeSliceAndReturn(): get %v, expect [0, 1]", s)
+	}
+	if pAddrAfter == pAddrBefore {
+		t.Errorf("changeSliceAndReturn(): expect new slice allocation, get new pointer %s and old one %s", pAddrAfter, pAddrBefore)
+	}
+
+	// apply same steps to append()
+	a := []int{0}
+	aAddrBefore := fmt.Sprintf("%p", &s[0])
+	a = append(a, 1) // returns same slice
+	aAddrAfter := fmt.Sprintf("%p", &s[0])
+	if aAddrAfter != aAddrBefore {
+		t.Errorf("append(): expect no new slice allocation, get new pointer %s and old one %s", aAddrAfter, aAddrBefore)
 	}
 
 }
