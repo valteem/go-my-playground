e0a7ca380f75bcf4ad807c39ee8d0067ef4e1261
valteem <val.teem.scn@gmail.com>
2024-08-04 20:07:37 +0300

    reuse: add several lint:ignore dirs

--- reuse/slice_as_func_arg.go
+++ reuse/slice_as_func_arg.go
@@ -1,6 +1,7 @@
 package reuse
 
 func AppendToSlice[T any](s []T, a T) {
+	//lint:ignore SA4006 for demo purpose only
 	s = append(s, a) // length and capacity of 's' are not affected by 'append' after function is completed
 }
 
@@ -8,7 +9,7 @@ func AppendToSliceP[T any](s *[]T, a T) {
 	*s = append(*s, a) // (*s) is a slice, since s is a pointer (?)
 }
 
-type SliceContainer [T any] struct {
+type SliceContainer[T any] struct {
 	s []T
 }
 
@@ -20,11 +21,12 @@ func (sc SliceContainer[T]) Slice() []T {
 	return sc.s
 }
 
-func (sc SliceContainer[T])AppendToSlice(a T) {
+func (sc SliceContainer[T]) AppendToSlice(a T) {
+	//lint:ignore SA4005 for demo purpose only
 	sc.s = append(sc.s, a)
 }
 
-func (sc *SliceContainer[T])AppendToSliceP(a T) {
+func (sc *SliceContainer[T]) AppendToSliceP(a T) {
 	sc.s = append(sc.s, a)
 }
 
@@ -33,8 +35,10 @@ func SlicesEqual[T comparable](s1 []T, s2 []T) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
-	for i := 0; i <  len(s1); i++ {
-		if s1[i] != s2[i] {return false}
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			return false
+		}
 	}
 	return true
 }
