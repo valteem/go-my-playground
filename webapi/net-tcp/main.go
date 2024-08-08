911f61a3ed29fd413fb539ec080aaaf28d603fea
valteem <val.teem.scn@gmail.com>
2024-08-08 20:53:14 +0300

    webapi/net-tcp: read from connection to small size byte buffer

--- webapi/net-tcp/main.go
+++ webapi/net-tcp/main.go
@@ -8,6 +8,10 @@ import (
 	"net"
 )
 
+const (
+	BUF_SIZE = 16
+)
+
 func main() {
 
 	l, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 44566})
@@ -21,12 +25,19 @@ func main() {
 			log.Fatalf("error listening: %s", err)
 		}
 		go func() {
-			b := make([]byte, 1024)
-			conn.Read(b)
-			if err != nil {
-				log.Fatalf("error reading from connection: %s", err)
+			r := make([]byte, 0)
+			b := make([]byte, BUF_SIZE)
+			for {
+				n, err := conn.Read(b)
+				if err != nil {
+					log.Fatalf("error reading from connection: %s", err)
+				}
+				r = append(r, b[:n]...)
+				if n < BUF_SIZE {
+					break
+				}
 			}
-			fmt.Println(string(b[:]))
+			fmt.Println(string(r[:]))
 		}()
 	}
 }
