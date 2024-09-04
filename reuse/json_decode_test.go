a5da43885eb0042a94780b3e571b5fbd09260d56
valteem <val.teem.scn@gmail.com>
2024-09-04 11:29:11 +0300

    reuse: complete json.Decode() example

--- reuse/json_decode_test.go
+++ reuse/json_decode_test.go
@@ -1,8 +1,11 @@
+// Decode() vs Unmarshal(): https://stackoverflow.com/a/21198571
+
 package reuse_test
 
 import (
 	"encoding/json"
 	"io"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -15,11 +18,15 @@ type Item struct {
 func TestJSONDecode(t *testing.T) {
 	//	input := "POST\r\nHTTP 1.1\r\nContent-Type: application/json\r\n\r\n{\"ID\":1001,\"Desc\":\"Some Name\"}"
 	input := "{\"ID\":1001,\"Desc\":\"Some Name\"}"
-	var it Item
+	var output Item
+	expectedOutput := Item{ID: 1001, Desc: "Some Name"}
 	decoder := json.NewDecoder(strings.NewReader(input))
-	err := decoder.Decode(&it)
+	err := decoder.Decode(&output)
 	if err != nil {
 		t.Errorf("error decoding input string: %v", err)
+	}
+	if !reflect.DeepEqual(output, expectedOutput) {
+		t.Errorf("error decoding JSON:\nget\n%v\nexpect\n%v\n", output, expectedOutput)
 	}
 }
 
