bc659b5d09fedda7f7cfea24ff00ed00aa82b46c
valteem <val.teem.scn@gmail.com>
2024-12-02 15:26:22 +0300

    webapi/ae-scs-sessions: move client part to dedicated function

--- webapi/ae-scs-sessions/sessions_test.go
+++ webapi/ae-scs-sessions/sessions_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -27,16 +28,12 @@ func runServer(sm *scs.SessionManager) {
 	}
 
 }
-func TestSessions(t *testing.T) {
 
-	sm := scs.New()
-	sm.Lifetime = 24 * time.Hour
-
-	go runServer(sm)
+func runClient() (string, error) {
 
 	jar, err := cookiejar.New(nil) // no option
 	if err != nil {
-		t.Fatalf("failed to create cookie jar: %v", err)
+		return "", fmt.Errorf("failed to create cookie jar: %v", err)
 	}
 	client := http.Client{
 		Jar: jar,
@@ -45,31 +42,46 @@ func TestSessions(t *testing.T) {
 	urlCreate := "http://localhost" + srvPort + "/create"
 	reqCreate, err := http.NewRequest(http.MethodPost, urlCreate, nil)
 	if err != nil {
-		t.Fatalf("failed to create a /create request: %v", err)
+		return "", fmt.Errorf("failed to create a /create request: %v", err)
 	}
 	_, err = client.Do(reqCreate)
 	if err != nil {
-		t.Fatalf("failed to get a response to /create request: %v", err)
+		return "", fmt.Errorf("failed to get a response to /create request: %v", err)
 	}
 
 	urlReceive := "http://localhost" + srvPort + "/receive"
 	reqReceive, err := http.NewRequest(http.MethodGet, urlReceive, nil)
 	if err != nil {
-		t.Fatalf("failed to create a /receive request: %v", err)
+		return "", fmt.Errorf("failed to create a /receive request: %v", err)
 	}
 	resp, err := client.Do(reqReceive)
 	if err != nil {
-		t.Fatalf("failed to get a response to /receive request: %v", err)
+		return "", fmt.Errorf("failed to get a response to /receive request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	bodyBytes, err := io.ReadAll(resp.Body)
+	msgBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		t.Fatalf("failed to read response body: %v", err)
+		return "", fmt.Errorf("failed to read response body: %v", err)
 	}
-	bodyStr := string(bodyBytes)
-	if bodyStr != msgBody {
-		t.Errorf("response message: get %q, expect %q", bodyStr, msgBody)
+	msgStr := string(msgBytes)
+
+	return msgStr, nil
+
+}
+func TestSessions(t *testing.T) {
+
+	sm := scs.New()
+	sm.Lifetime = 24 * time.Hour
+
+	go runServer(sm)
+
+	msg, err := runClient()
+	if err != nil {
+		t.Fatalf("failed to run client part: %v", err)
+	}
+	if msg != msgBody {
+		t.Errorf("response message: get %q, expect %q", msg, msgBody)
 	}
 
 }
