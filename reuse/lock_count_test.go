0153f723c6bd6bab260996678b7b81c3c362f28a
valteem <val.teem.scn@gmail.com>
2024-12-18 19:13:07 +0300

    reuse: rework lock counter example, add atomic counter, add benchmarks for mutex and atomic

--- reuse/lock_count_test.go
+++ reuse/lock_count_test.go
@@ -2,38 +2,114 @@ package reuse_test
 
 import (
 	"sync"
+	"sync/atomic"
 	"testing"
-	"time"
 )
 
 var (
 	lockedCounter              = 0
 	countLock                  sync.Mutex
+	atomicCounter              atomic.Int64
+	numWorkers                 = 1000
 	numCallsToIncrementCounter = 1000
 )
-
-func incrementLockedCounter(timeout time.Duration, wg *sync.WaitGroup) {
-	countLock.Lock()
-	lockedCounter++
-	countLock.Unlock()
-	time.Sleep(timeout)
-	wg.Done()
-}
 
 func TestLockedCounter(t *testing.T) {
 
 	var wg sync.WaitGroup
-	wg.Add(numCallsToIncrementCounter)
+	wg.Add(numWorkers)
 
-	timeout := 5 * time.Second
-	for i := 0; i < numCallsToIncrementCounter; i++ {
-		go incrementLockedCounter(timeout, &wg)
+	for i := 0; i < numWorkers; i++ {
+		go func(wg *sync.WaitGroup) {
+			for i := 0; i < numCallsToIncrementCounter; i++ {
+				countLock.Lock()
+				lockedCounter++
+				countLock.Unlock()
+			}
+			wg.Done()
+		}(&wg)
+
 	}
 
 	wg.Wait()
 
-	if lockedCounter != numCallsToIncrementCounter {
-		t.Errorf("counter: get %d, expect %d", lockedCounter, numCallsToIncrementCounter)
+	if lockedCounter != numWorkers*numCallsToIncrementCounter {
+		t.Errorf("counter: get %d, expect %d", lockedCounter, numWorkers*numCallsToIncrementCounter)
 	}
+
+}
+
+// Atomics faster than mutexes
+// https://stackoverflow.com/a/47446114
+func TestAtomicCounter(t *testing.T) {
+
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := 0; i < numWorkers; i++ {
+		go func(wg *sync.WaitGroup) {
+			for i := 0; i < numCallsToIncrementCounter; i++ {
+				atomicCounter.Add(1)
+			}
+			wg.Done()
+		}(&wg)
+	}
+
+	wg.Wait()
+
+	if actual, expected := atomicCounter.Load(), int64(numWorkers*numCallsToIncrementCounter); actual != expected {
+		t.Errorf("counter value:\nget\n%d\nexpect\n%d", actual, expected)
+	}
+
+}
+
+func BenchmarkCounter(b *testing.B) {
+
+	b.Run("mutex", func(b *testing.B) {
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(wg *sync.WaitGroup) {
+				for i := 0; i < numCallsToIncrementCounter; i++ {
+					countLock.Lock()
+					lockedCounter++
+					countLock.Unlock()
+				}
+				wg.Done()
+			}(&wg)
+		}
+		wg.Wait()
+	})
+
+	b.Run("atomic", func(b *testing.B) {
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+		for i := 0; i < b.N; i++ {
+			go func(wg *sync.WaitGroup) {
+				for i := 0; i < numCallsToIncrementCounter; i++ {
+					atomicCounter.Add(1)
+				}
+				wg.Done()
+			}(&wg)
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkCounterNoConcurrency(b *testing.B) {
+
+	b.Run("mutex", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			countLock.Lock()
+			lockedCounter++
+			countLock.Unlock()
+		}
+	})
+
+	b.Run("atomic", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			atomicCounter.Add(1)
+		}
+	})
 
 }
