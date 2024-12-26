9dc99c38577319ad7ec257e4caf5d790353ad3a0
valteem <val.teem.scn@gmail.com>
2024-12-26 16:40:31 +0300

    reuse: switch control flow
    only first matching case is executed

---
+++ reuse/switch_control_flow_test.go
@@ -0,0 +1,38 @@
+package reuse_test
+
+import (
+	"slices"
+	"testing"
+)
+
+func switchNumberRange(input int) []string {
+	output := []string{}
+	switch {
+	case input != 0:
+		output = append(output, "non-zero")
+	case input > 0:
+		output = append(output, "positive")
+	case (input/2)*2 == input:
+		output = append(output, "even")
+	case (input/2)*2 < input:
+		output = append(output, "uneven")
+	}
+	return output
+}
+
+func TestSwitchNaumberRange(t *testing.T) {
+
+	tests := []struct {
+		input  int
+		output []string
+	}{
+		{1, []string{"non-zero"}}, // only first matching case is executed
+	}
+
+	for _, tc := range tests {
+		if output := switchNumberRange(tc.input); !slices.Equal(output, tc.output) {
+			t.Errorf("get\n%v\nexpect\n%v\n", output, tc.output)
+		}
+	}
+
+}
