f008a15355880b6258201654cfb3c0ca244a1798
valteem <val.teem.scn@gmail.com>
2024-12-30 12:57:57 +0300

    webapi/gin-binding-internal: SetWithProperType()

--- webapi/gin-binding-internal/core.go
+++ webapi/gin-binding-internal/core.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -11,4 +14,29 @@ func HeadTail(input, separator string) (string, string) {
 		return input, ""
 	}
 	return input[:index], input[index+len(separator):]
+}
+
+func SetIntField(input string, bitSize int, field reflect.Value) error {
+	if input == "" {
+		input = "0"
+	}
+	intVal, err := strconv.ParseInt(input, 10, bitSize)
+	if err != nil {
+		return err
+	}
+	if !field.CanSet() {
+		return fmt.Errorf("cannot set value")
+	}
+	field.SetInt(intVal)
+	return nil
+}
+
+func SetWithProperType(input string, value reflect.Value, field reflect.StructField) error {
+	switch value.Kind() {
+	case reflect.Int:
+		return SetIntField(input, 0, value)
+	case reflect.String:
+		value.SetString(input)
+	}
+	return nil
 }
--- webapi/gin-binding-internal/core_test.go
+++ webapi/gin-binding-internal/core_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +21,32 @@ func TestHeadTail(t *testing.T) {
 		if head, tail := HeadTail(tc.input, tc.sep); head != tc.head || tail != tc.tail {
 			t.Errorf("head/tail for %s:%s: get  %s/%s, expect %s/%s", tc.input, tc.sep, head, tail, tc.head, tc.tail)
 		}
+	}
+}
+
+// https://stackoverflow.com/a/48874650
+// Interface type variables are not addressable and cannot be set with Value.Set() methods
+func TestSetWithProperType(t *testing.T) {
+
+	field := reflect.StructField{}
+
+	targetStr := ""
+	inputStr := "apples"
+	err := SetWithProperType(inputStr, reflect.ValueOf(&targetStr).Elem(), field)
+	if err != nil {
+		t.Fatalf("failed to assign %q: %v", inputStr, err)
+	}
+	if targetStr != inputStr {
+		t.Errorf("get %q, expect %q", targetStr, inputStr)
+	}
+
+	targetInt := 0
+	inputInt := "42"
+	err = SetWithProperType(inputInt, reflect.ValueOf(&targetInt).Elem(), field)
+	if err != nil {
+		t.Fatalf("failed to assign %q: %v", inputStr, err)
+	}
+	if targetInt != 42 {
+		t.Errorf("get %d, expect %d", targetInt, 42)
 	}
 }
