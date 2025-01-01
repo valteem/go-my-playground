de935af06b8b17fa59328d11005040aadc853249
valteem <val.teem.scn@gmail.com>
2025-01-01 18:30:52 +0300

    reuse: compare two custom errors

--- reuse/error_unwrap_test.go
+++ reuse/error_unwrap_test.go
@@ -3,6 +3,8 @@ package reuse_test
 import (
 	"errors"
 	"fmt"
+	"net/url"
+	//	"reflect"
 	"testing"
 
 	"github.com/valteem/reuse"
@@ -17,4 +19,30 @@ func TestErrorUnwrap(t *testing.T) {
 	if errors.Unwrap(err) == nil {
 		fmt.Println("no more errors inside")
 	}
+}
+
+func TestCustomErrorComparison(t *testing.T) {
+
+	input := "some invalid URL string"
+
+	e1 := &url.Error{
+		Op:  "parse",
+		URL: input,
+		Err: errors.New("invalid URI for request")}
+
+	e2 := &url.Error{
+		Op:  "parse",
+		URL: input,
+		Err: errors.New("invalid URI for request")}
+
+	if errors.Is(e1, e2) {
+		t.Errorf("expect errors not considered equal")
+	}
+
+	// avoid using reflect.DeepEqual with errors
+	/*
+		if !reflect.DeepEqual(e1, e2) {
+			t.Errorf("should be considered equal")
+		}
+	*/
 }
