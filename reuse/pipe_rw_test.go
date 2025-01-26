072485642f8988ffd3b48a75a13f9dcd7a892ed7
valteem <val.teem.scn@gmail.com>
2025-01-26 08:41:31 +0300

    reuse: tweak io.Pipe() test

--- reuse/pipe_rw_test.go
+++ reuse/pipe_rw_test.go
@@ -10,33 +10,38 @@ import (
 )
 
 func TestPipeRW(t *testing.T) {
-	r, w := io.Pipe()
-	buf := bytes.NewBuffer(nil)
-	msgWrite, msgRead := "abc", "abcabc"
-	var wg sync.WaitGroup
-	wg.Add(2)
 
-	go func(wg *sync.WaitGroup) {
-		fmt.Fprint(w, msgWrite)
-		wg.Done()
-	}(&wg)
-	go func(wg *sync.WaitGroup) {
-		fmt.Fprint(w, msgWrite)
-		wg.Done()
-	}(&wg)
+	r, w := io.Pipe()
+	defer w.Close()
+
+	buf := bytes.NewBuffer(nil)
+
+	msgWrite, msgRead := "abc", ""
+
+	var wg sync.WaitGroup
+	wg.Add(100)
 
 	go func() {
+		// io.Copy(dst, src) copies from src to dst until EOF is reached
 		_, e := io.Copy(buf, r)
 		if e != nil {
 			t.Errorf("Error copying to buffer: %s", e)
 		}
-		if output := buf.String(); output != msgRead {
-			t.Errorf("Reading from buffer: get %s, expect %s", output, msgRead)
-		}
 	}()
 
+	for i := 0; i < 100; i++ {
+		go func() {
+			fmt.Fprint(w, msgWrite)
+			wg.Done()
+		}()
+		msgRead += msgWrite
+	}
+
 	wg.Wait()
-	go func() { w.Close() }()
+
+	if output := buf.String(); output != msgRead {
+		t.Errorf("Reading from buffer: get %s, expect %s", output, msgRead)
+	}
 
 }
 
@@ -54,5 +59,20 @@ func TestPipeWrite(t *testing.T) {
 	}
 	if output := buf.String(); output != m {
 		t.Errorf("Read from buffer: get %s, expect %s", output, m)
+	}
+}
+
+func TestEmptyPipe(t *testing.T) {
+
+	r, w := io.Pipe()
+	w.Close()
+
+	buf := &strings.Builder{}
+
+	_, err := io.Copy(buf, r)
+
+	if err != nil {
+		t.Fatalf("failed to read from empty PipeReader: %v", err)
+
 	}
 }
