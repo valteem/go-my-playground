7e5c80bd5614c3e51d24fb37b8ffbf33aca0cf0e
valteem <val.teem.scn@gmail.com>
2025-03-26 17:13:01 +0700

    examine/validator: tweak basic example

--- examine/validator/basic/basic.go
+++ examine/validator/basic/basic.go
@@ -3,6 +3,6 @@ package basic
 type Person struct {
 	GivenName  string `validate:"required"`
 	FamilyName string `validate:"required"`
-	Age        int    `validate:"gte=0,lte=123"`
+	Age        uint   `validate:"gte=0,lte=123"`
 	Email      string `validate:"required,email"`
 }
