e05dd943f84a22fdfdf84e782d0aaa99b6ce9c0d
valteem <val.teem.scn@gmail.com>
2025-04-08 14:00:19 +0700

    my-snippets/loop-variable-capture: elaborate on the nature of the bug

--- my-snippets/loop-variable-capture/1.22/main.go
+++ my-snippets/loop-variable-capture/1.22/main.go
@@ -5,8 +5,24 @@ import (
 )
 
 func main() {
+	LoopVarInside()
+	LoopVarOutside()
+}
+
+func LoopVarInside() {
 	var prints []func()
-	for i := 1; i <= 3; i++ {
+	for i := 1; i <= 3; i++ { // loop variable bug fixed - i is new int instance at each iteration
+		prints = append(prints, func() { fmt.Println(i) })
+	}
+	for _, print := range prints {
+		print()
+	}
+}
+
+func LoopVarOutside() {
+	var prints []func()
+	var i int
+	for i = 1; i <= 3; i++ { // i is same across all iterations - 'bug' is still there
 		prints = append(prints, func() { fmt.Println(i) })
 	}
 	for _, print := range prints {
