14cd8a927ae8ac1e330ccad55a5cc66e644ccc26
valteem <val.teem.scn@gmail.com>
2025-04-27 15:26:02 +0300

    webapi/product-catalog: add hashing implementation

--- webapi/product-catalog/hashing/hashing.go
+++ webapi/product-catalog/hashing/hashing.go
@@ -1,5 +1,26 @@
 package hashing
 
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
 type Hasher interface {
 	Hash(string) string
+}
+
+type SHA1Hasher struct {
+	salt string
+}
+
+func NewSHA1Hasher(salt string) *SHA1Hasher {
+	return &SHA1Hasher{salt: salt}
+}
+
+func (h *SHA1Hasher) Hash(input string) string {
+	
+	hash := sha1.New()
+	hash.Write([]byte(input))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(h.salt)))
 }
