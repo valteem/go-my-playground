2cbe19f888c2d76df6e0fa58c29a6449d60ee593
valteem <val.teem.scn@gmail.com>
2025-05-17 15:32:24 +0300

    reuse/chann: replace nil out of copied channel reference with bool flag

--- reuse/chann/chan_test.go
+++ reuse/chann/chan_test.go
@@ -58,3 +58,58 @@ func TestReadOK(t *testing.T) {
 	}
 
 }
+
+func TestReadOKWithFlag(t *testing.T) {
+
+	numWrites := 5
+	wg := sync.WaitGroup{}
+	wg.Add(numWrites)
+
+	ch := make(chan int)
+	go func() {
+		for i := range numWrites {
+			ch <- i
+			wg.Done()
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	// drain channel in case of premature return
+	defer func() {
+		if ch != nil {
+			for range ch {
+			}
+		}
+	}()
+
+	var flag bool
+	output := []int{}
+	phantomReads := 0
+
+	for {
+		select {
+		case v, ok := <-ch:
+			if !ok {
+				// replace nil out of copied channel reference to bool flag
+				flag = true
+				break
+			}
+			output = append(output, v)
+		default:
+			phantomReads++
+		}
+		if flag {
+			break
+		}
+
+	}
+
+	if len(output) != numWrites {
+		t.Errorf("output: get %d values, expect %d", len(output), numWrites)
+	}
+
+}
