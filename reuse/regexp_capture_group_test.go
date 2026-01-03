cebc320852179b51930854038e585ecf4dd351d7
valteem <val.teem.scn@gmail.com>
2026-01-03 15:56:29 +0300

    reuse: elaborate on capturing multiple regexp groups

--- reuse/regexp_capture_group_test.go
+++ reuse/regexp_capture_group_test.go
@@ -1,6 +1,7 @@
 package reuse_test
 
 import (
+	"reflect"
 	"regexp"
 
 	"testing"
@@ -38,6 +39,36 @@ func TestCaptureGroup(t *testing.T) {
 				t.Errorf("capture group %q^ get %q, expect %q", tc.captureGroup, match[i], tc.output)
 			}
 		}
-
 	}
+}
+
+func TestMultipleCapture(t *testing.T) {
+
+	tests := []struct {
+		pattern string
+		input   string
+		output  map[string]string
+	}{
+		{
+			pattern: `(?P<num1>\d+) (?P<text1>\D+) (?P<num2>\d+) (?P<text2>.*)`,
+			input:   `123 onions 456 apples`,
+			output:  map[string]string{"num1": "123", "text1": "onions", "num2": "456", "text2": "apples"},
+		},
+	}
+
+	for _, tc := range tests {
+		re = regexp.MustCompile(tc.pattern)
+		matches := re.FindStringSubmatch(tc.input) // captures nothing if input does not fully match pattern
+		names := re.SubexpNames()
+		captures := map[string]string{}
+		for i, n := range names {
+			if i > 0 {
+				captures[n] = matches[i]
+			}
+		}
+		if !reflect.DeepEqual(captures, tc.output) {
+			t.Errorf("get\n%v\nexpect\n%v\n", captures, tc.output)
+		}
+	}
+
 }
